a43b482573a156acff913c7bb5695e562c71fd1c
Tishly <ekaetteessien27@gmail.com>
2023-01-07 12:04:58 +0100

    Corrected function call

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,6 +7,7 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """
-    A function that takes a list of floats and returns the sum of all its components
+    A function that takes a list of floats and
+    returns the sum of all its components
     """
     return sum(input_list)
