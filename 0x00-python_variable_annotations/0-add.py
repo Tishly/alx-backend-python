b78408bd722290bfbddc2039ae31e058483bf995
Tishly <ekaetteessien27@gmail.com>
2023-01-07 08:19:58 +0100

    Annotation of a function to return the sum of two floats

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/ python3
-"""A function that returns the sum of two floats"""
+"""A python file to demonstrate annotation"""
 
 
 def add(a: float, b: float) -> float:
+    """A function that returns the sum of two floats"""
     return a + b
