87a8e52eba190d2e9e21ffb2301063618754117b
Tishly <ekaetteessien27@gmail.com>
2023-02-07 17:21:01 +0100

    Basic async syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
     """
     an asynchronous coroutine that takes in an integer argument,
     waits some seconds and returns it
