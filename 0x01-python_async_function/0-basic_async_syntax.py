43a2989e443e6e75b8ecb73b9437e24fe52e386f
Tishly <ekaetteessien27@gmail.com>
2023-02-07 11:12:25 +0100

    Basic async syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,8 +11,10 @@ async def wait_random(max_delay = 10):
     an asynchronous coroutine that takes in an integer argument,
     waits some seconds and returns it
     """
-    await asynrandom.uniform(0, 11)
+    await asyncio.sleep(random.uniform(0, 11))
     return max_delay
 
 
-async.run(wait_random())
+if __name__ == "__main__":
+    asyncio.run(wait_random())
+
