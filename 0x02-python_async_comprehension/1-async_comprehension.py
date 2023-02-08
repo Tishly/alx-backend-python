1a986513af04f64d7348dbd3b431210a919b9600
Tishly <ekaetteessien27@gmail.com>
2023-02-08 04:48:12 +0100

    Async comprehension of a generator function

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Async Comprehension
+"""
+import asyncio
+import random
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    A Coroutine that collects 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers
+    """
+    result = [i async for i in async_generator()]
+    return result
+
+
+if __name__ == "__main__":
+    asyncio.run(async_comprehension())
